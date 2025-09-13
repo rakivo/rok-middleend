40175e83f0dcd52fdfb4a24916fb06165ef0f357
Mark Tyrkba <awfulmarco@gmail.com>
2025-09-13 15:27:17 +0300

    docs: update lower_fib example to use instruction comments

--- examples/lower_fib.rs
+++ examples/lower_fib.rs
@@ -23,28 +23,28 @@ fn main() {
     let ret_n_block = builder.create_block();
     let exit_block = builder.create_block();
 
-    let v1 = builder.ins().iconst(Type::I64, 2);
-    let v2 = builder.ins().ilt(arg_n, v1);
-    builder.ins().brif(v2, ret_n_block, recur_block);
+    let v1 = builder.ins().iconst_with_comment(Type::I64, 2, "load constant 2");
+    let v2 = builder.ins().ilt_with_comment(arg_n, v1, "n < 2");
+    builder.ins().brif_with_comment(v2, ret_n_block, recur_block, "if n < 2, return n, else recurse");
 
     builder.switch_to_block(recur_block);
-    let v1_minus = builder.ins().iconst(Type::I64, 1);
-    let v3 = builder.ins().isub(arg_n, v1_minus);
-    let v4 = builder.ins().call(fib_id, &[v3])[0];
-    let v5 = builder.ins().iconst(Type::I64, 2);
-    let v6 = builder.ins().isub(arg_n, v5);
-    let v7 = builder.ins().call(fib_id, &[v6])[0];
-    let v8 = builder.ins().iadd(v4, v7);
-    builder.ins().stack_store(result_slot, v8);
-    builder.ins().jump(exit_block);
+    let v1_minus = builder.ins().iconst_with_comment(Type::I64, 1, "load constant 1");
+    let v3 = builder.ins().isub_with_comment(arg_n, v1_minus, "n - 1");
+    let v4 = builder.ins().call_with_comment(fib_id, &[v3], "fib(n - 1)")[0];
+    let v5 = builder.ins().iconst_with_comment(Type::I64, 2, "load constant 2");
+    let v6 = builder.ins().isub_with_comment(arg_n, v5, "n - 2");
+    let v7 = builder.ins().call_with_comment(fib_id, &[v6], "fib(n - 2)")[0];
+    let v8 = builder.ins().iadd_with_comment(v4, v7, "fib(n - 1) + fib(n - 2)");
+    builder.ins().stack_store_with_comment(result_slot, v8, "store result");
+    builder.ins().jump_with_comment(exit_block, "jump to exit");
 
     builder.switch_to_block(ret_n_block);
-    builder.ins().stack_store(result_slot, arg_n);
-    builder.ins().jump(exit_block);
+    builder.ins().stack_store_with_comment(result_slot, arg_n, "store n");
+    builder.ins().jump_with_comment(exit_block, "jump to exit");
 
     builder.switch_to_block(exit_block);
-    let res = builder.ins().stack_load(Type::I64, result_slot);
-    builder.ins().ret(&[res]);
+    let res = builder.ins().stack_load_with_comment(Type::I64, result_slot, "load result");
+    builder.ins().ret_with_comment(&[res], "return result");
 
     builder.finalize();
     module.define_function(fib_id);
